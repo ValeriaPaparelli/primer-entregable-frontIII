c1736832f0d6bc44f2a8403bc88c405b5ff5fbe7
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-03-09 19:33:12 -0300

    primer commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Formulario</h1>
+      <Card />
+    </div>
+  );
+};
+
+export default Form;
