5e585e2bc22742be5eca324a9eebdbc512b0e39a
Valeria Paparelli <valeria.paparelli@live.com.ar>
2023-03-10 00:02:15 -0300

    formulario de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Form from "./components/Form/Form";
 
 function App() {
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "./Card.css";
 
-const Card = () => {
+const Card = ({ userName, email, password }) => {
   return (
-    <div>
-      <h3>Card</h3>
+    <div className="container-card">
+      <h2>¡Felicitaciones {userName}!!!</h2>
+      <h3>Tu registro ha sido exitoso</h3>
+      <h4>Tu email para ingresar es: {email}</h4>
+      <h4>Contraseña: {password}</h4>
     </div>
   );
 };
